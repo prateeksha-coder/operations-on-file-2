7528ad4cf057669197397b12cf94d3ae6e60a338
prateeksha-coder <prateeksha.codingal@gmail.com>
2026-04-20 13:08:53 +0530

    commit

--- a1.py
+++ a1.py
@@ -2,6 +2,7 @@
 with open('Codingal.txt', 'w') as file:
   file.write("Hi! I am Penguin and I am 1 yr old.\n Hello everyone.\nWelcome to Codingal")
 
+
 # split file into words
 with open('Codingal.txt', 'r') as file:
   data = file.readlines()
